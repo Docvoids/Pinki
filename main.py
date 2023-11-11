9330e78bfb98b35b66bc80ead558582b20255d32
DocVoids <rgordienko872@gmail.com>
2023-11-11 19:51:41 +0200

    Add Hello and mem

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello world")
+import telebot
+
+bot=telebot.TeleBot('6830689745:AAFyWR7zuWFIiA9JbOcz2QXf6_UkUfzZjaA')
+@bot.message_handler(commands=['start','hello!'])
+def hello(messege):
+    bot.send_message(messege.chat.id, '<b>Hello!(UwU)</b>', parse_mode='html')
+
+@bot.message_handler(commands=['best'])
+def hello(messege):
+    bot.send_message(messege.chat.id, f'IM-32 , (and your mom ,{messege.from_user.first_name}))))')
+
+
+
+bot.polling(none_stop=True)
