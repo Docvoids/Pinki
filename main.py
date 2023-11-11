2a9c0f277cc9be4e9dbc6555a77a334c5dd3d6d6
Docvoids <143121183+Docvoids@users.noreply.github.com>
2023-11-11 21:24:40 +0200

    Sec.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import telebot
 
-bot=telebot.TeleBot('6830689745:AAFyWR7zuWFIiA9JbOcz2QXf6_UkUfzZjaA')
+bot=telebot.TeleBot('not for you')
 @bot.message_handler(commands=['start','hello!'])
 def hello(messege):
     bot.send_message(messege.chat.id, '<b>Hello!(UwU)</b>', parse_mode='html')
